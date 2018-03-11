26caac1caf794c2f22c36a41041a6ee42b101847
Elb4t <eloy4t@gmail.com>
2018-03-11 11:04:27 +0100

    Integrar EventosWeb en Eventos

--- Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
+++ Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
@@ -1,20 +1,19 @@
 package es.elb4t.eventosweb
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
+
+
 
 
 class ActividadPrincipal : AppCompatActivity() {
+    lateinit var navegador: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actividad_principal)
-        val intent = Intent(Intent.ACTION_VIEW)
-        val uri = Uri.parse(
-                "https://eventos-3161f.firebaseapp.com/index.html")
-        intent.data = uri
-        startActivity(intent)
+        navegador = findViewById(R.id.webkit)
+        navegador.loadUrl("https://eventos-3161f.firebaseapp.com/index.html")
     }
 }
