67fbae6a2e3e4861d1d28095068e245654efbb74
Elb4t <eloy4t@gmail.com>
2018-03-11 12:08:30 +0100

    Añadir funcionalidad página siguiente, anterior y detener carga

--- Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
+++ Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
 import android.widget.ProgressBar
 
 
@@ -15,11 +16,17 @@ class ActividadPrincipal : AppCompatActivity() {
     lateinit var navegador: WebView
     private lateinit var barraProgreso: ProgressBar
     var dialogo: ProgressDialog? = null
+    lateinit var btnDetener: Button
+    lateinit var btnAnterior:Button
+    lateinit var btnSiguiente:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actividad_principal)
         barraProgreso = findViewById(R.id.barraProgreso)
+        btnDetener = findViewById(R.id.btnDetener)
+        btnAnterior = findViewById(R.id.btnAnterior)
+        btnSiguiente = findViewById(R.id.btnSiguiente)
         navegador = findViewById(R.id.webkit)
         navegador.settings.javaScriptEnabled = true
         navegador.settings.builtInZoomControls = false
@@ -51,11 +58,27 @@ class ActividadPrincipal : AppCompatActivity() {
                 dialogo!!.setMessage("Cargando...")
                 dialogo!!.setCancelable(true)
                 dialogo!!.show()
+                btnDetener.isEnabled = true
             }
 
             override fun onPageFinished(view: WebView, url: String) {
                 dialogo!!.dismiss()
+                btnDetener.isEnabled = false
+                btnAnterior.isEnabled = view.canGoBack()
+                btnSiguiente.isEnabled = view.canGoForward()
             }
         }
+    }
+
+    fun detenerCarga(v: View) {
+        navegador.stopLoading()
+    }
+
+    fun irPaginaAnterior(v: View) {
+        navegador.goBack()
+    }
+
+    fun irPaginaSiguiente(v: View) {
+        navegador.goForward()
     }
 }
