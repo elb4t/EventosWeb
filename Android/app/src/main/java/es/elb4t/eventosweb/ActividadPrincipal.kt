968244c1e91acbd3761f101f3cad5a72a24e7c36
Elb4t <eloy4t@gmail.com>
2018-03-11 12:03:13 +0100

    Añadir diálogo de carga

--- Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
+++ Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
@@ -1,5 +1,7 @@
 package es.elb4t.eventosweb
 
+import android.app.ProgressDialog
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -9,11 +11,10 @@ import android.webkit.WebViewClient
 import android.widget.ProgressBar
 
 
-
-
 class ActividadPrincipal : AppCompatActivity() {
     lateinit var navegador: WebView
     private lateinit var barraProgreso: ProgressBar
+    var dialogo: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +25,16 @@ class ActividadPrincipal : AppCompatActivity() {
         navegador.settings.builtInZoomControls = false
         navegador.loadUrl("file:///android_asset/index.html")
         //navegador.loadUrl("https://eventos-3161f.firebaseapp.com/index.html")
-        navegador.webViewClient = object :WebViewClient(){
+        navegador.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                var url_filtro:String = "http://www.androidcurso.com/"
-                if (url != url_filtro){
+                var url_filtro: String = "http://www.androidcurso.com/"
+                if (url != url_filtro) {
                     view.loadUrl(url_filtro)
                 }
                 return false
             }
         }
-        navegador.webChromeClient = object : WebChromeClient(){
+        navegador.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(view: WebView, progreso: Int) {
                 barraProgreso.progress = 0
                 barraProgreso.visibility = View.VISIBLE
@@ -42,6 +43,18 @@ class ActividadPrincipal : AppCompatActivity() {
                 if (progreso == 100) {
                     barraProgreso.visibility = View.GONE
                 }
+            }
+        }
+        navegador.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                dialogo = ProgressDialog(this@ActividadPrincipal)
+                dialogo!!.setMessage("Cargando...")
+                dialogo!!.setCancelable(true)
+                dialogo!!.show()
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                dialogo!!.dismiss()
             }
         }
     }
