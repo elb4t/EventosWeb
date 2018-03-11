d8ff3e3013ed128180b6c347c10e7348a325bfaa
Elb4t <eloy4t@gmail.com>
2018-03-11 11:37:41 +0100

    Añadir barra de progreso

--- Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
+++ Android/app/src/main/java/es/elb4t/eventosweb/ActividadPrincipal.kt
@@ -2,16 +2,23 @@ package es.elb4t.eventosweb
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+
+
 
 
 class ActividadPrincipal : AppCompatActivity() {
     lateinit var navegador: WebView
+    private lateinit var barraProgreso: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actividad_principal)
+        barraProgreso = findViewById(R.id.barraProgreso)
         navegador = findViewById(R.id.webkit)
         navegador.settings.javaScriptEnabled = true
         navegador.settings.builtInZoomControls = false
@@ -24,6 +31,17 @@ class ActividadPrincipal : AppCompatActivity() {
                     view.loadUrl(url_filtro)
                 }
                 return false
+            }
+        }
+        navegador.webChromeClient = object : WebChromeClient(){
+            override fun onProgressChanged(view: WebView, progreso: Int) {
+                barraProgreso.progress = 0
+                barraProgreso.visibility = View.VISIBLE
+                this@ActividadPrincipal.setProgress(progreso * 1000)
+                barraProgreso.incrementProgressBy(progreso)
+                if (progreso == 100) {
+                    barraProgreso.visibility = View.GONE
+                }
             }
         }
     }
